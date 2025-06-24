92084c622c6f97ba535e3ab55045ad6c37827352
Curtis Hamilton <hamiltcl@verizon.net>
2025-06-24 15:27:11 -0400

    Add files via upload

---
+++ drivers/gpu/drm/nouveau/include/nvif/os.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: MIT */
+#ifndef __NOUVEAU_OS_H__
+#define __NOUVEAU_OS_H__
+
+#include <linux/types.h>
+#include <linux/slab.h>
+#include <linux/mutex.h>
+#include <linux/pci.h>
+#include <linux/platform_device.h>
+#include <linux/printk.h>
+#include <linux/bitops.h>
+#include <linux/firmware.h>
+#include <linux/module.h>
+#include <linux/i2c.h>
+#include <linux/i2c-algo-bit.h>
+#include <linux/delay.h>
+#include <linux/io-mapping.h>
+#include <linux/acpi.h>
+#include <linux/vmalloc.h>
+#include <linux/dmi.h>
+#include <linux/reboot.h>
+#include <linux/interrupt.h>
+#include <linux/log2.h>
+#include <linux/pm_runtime.h>
+#include <linux/power_supply.h>
+#include <subdev/clk.h>
+#ifdef __linux__
+#include <linux/regulator/consumer.h>
+#endif
+#include <linux/agp_backend.h>
+#ifdef __linux__
+/#include <linux/reset.h>
+#endif
+#include <linux/iommu.h>
+#include <linux/of_device.h>
+
+#include <asm/unaligned.h>
+
+#ifdef __linux__
+#include <soc/tegra/fuse.h>
+#include <soc/tegra/pmc.h>
+#endif
+#endif
